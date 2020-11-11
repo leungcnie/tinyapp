a46bbf24b5be01ea0067761307620a4145a810b9
Connie <leungcnie@gmail.com>
2020-11-11 02:10:13 +0000

    Implemented POST /logout handler so that username cookie is cleared and user is redirected to /urls page

--- express_server.js
+++ express_server.js
@@ -21,6 +21,12 @@ app.post('/login', (req, res) => {
   res.redirect("/urls");
 })
 
+// POST /logout
+app.post('/logout', (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+})
+
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
